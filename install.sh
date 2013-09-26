3b9092f821ac311869db264bb0c8fdef0e1a939c
Elco Jacobs <elcojacobs@gmail.com>
2013-09-26 14:46:37 +0200

    corrected spelling error in echo: udo instead of sudo

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@
 
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root - udo ./install.sh)" 1>&2
+   echo "This script must be run as root: sudo ./install.sh)" 1>&2
    exit 1
 fi
 
