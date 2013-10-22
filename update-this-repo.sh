cb78acafaff55a830f23f90ef2639a5367ce6ea6
Elco Jacobs <elcojacobs@gmail.com>
2013-10-22 20:44:24 +0200

    cd to path before running git commands in update-this-repo.sh

--- update-this-repo.sh
+++ update-this-repo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-active_branch=$(git symbolic-ref -q HEAD)
-active_branch=${active_branch##refs/heads/}
-
 unset CDPATH
 myPath="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+cd "$myPath"
+
+active_branch=$(git symbolic-ref -q HEAD)
+active_branch=${active_branch##refs/heads/}
 
 git fetch
 changes=$(git log HEAD..origin/"$active_branch" --oneline)
@@ -21,10 +22,11 @@ case "$yn" in
         git pull;
         if [ $? -ne 0 ]; then
             echo ""
-            echo "An error occurred during git pull. Please update this repository manually."
-            echo  "You can stash your local changes and then pull: sudo git stash; sudo git pull"
+            echo "An error occurred during git pull. Please update $myPath manually."
+            echo  "You can stash your local changes and then pull: cd $myPath; sudo git stash; sudo git pull"
             echo  ""
         fi
         exit 1;;
     * ) exit 0;;
 esac
+cd -
